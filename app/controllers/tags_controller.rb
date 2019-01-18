611d9646caed01fdc87b3942c430ed8b2b8ce794
aburk3 <aburk3@gmail.com>
2019-01-17 19:42:24 -0600

    refactored tags controller so that I could remove an unneeded method

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,30 +3,11 @@ class TagsController < ApplicationController
   get '/tags/:id' do
 
     if logged_in?
-      @website = current_user.websites.find_by_id(params[:id])
-      @tags = current_user.websites.all.each do |website|
-        website.tag
-      end
-      binding.pry
-      ###########################################
-      #  ONLY SHOW CURRENT USERS WEBSITES/tags  #
-      ###########################################
-      erb :'tags/show_tag'
-    else
-      redirect to '/login'
-    end
-  end
-
-  get '/mytags/:id' do
-    uniq_tags
-    if logged_in?
-      @tag = Tag.where(:id => params[:id])
-      @website = @tag.first.websites.first
-      @tags = []
-
-      @tag_instances.each do |tag|
-        if tag.websites.any? && tag.id == params[:id].to_i
-          @tags << tag
+      @tag = Tag.find_by_id(params[:id])
+      @websites = []
+      @tag.websites.each do |website|
+        if website.user == current_user
+          @websites << website
         end
       end
       erb :'tags/show_tag'
@@ -36,7 +17,7 @@ class TagsController < ApplicationController
   end
 
   post '/tags' do
-    redirect to "/mytags/#{params[:dropdown]}"
+    redirect to "/tags/#{params[:dropdown]}"
   end
 
 end
